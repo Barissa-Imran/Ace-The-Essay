9069781c767f84bba4e40a6a7c041e59007ae2a4
Barissa-Imran <imranbarissa@gmail.com>
2021-12-12 16:55:22 +0300

    fix static

--- ATE/ATE/settings.py
+++ ATE/ATE/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 # -----static files config-------
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = "/media/"
