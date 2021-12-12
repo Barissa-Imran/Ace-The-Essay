e36fbb5c631270fae0c5dbb9e80aa3f1ec448a29
Barissa-Imran <imranbarissa@gmail.com>
2021-12-12 21:06:19 +0300

    add wsgi file to path

--- ATE/ATE/wsgi.py
+++ ATE/ATE/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
 from django.core.wsgi import get_wsgi_application
+
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ATE.settings')
 
