75a6b4ebbafbb856bd621e9de89d10296d78590e
Barissa-Imran <imranbarissa@gmail.com>
2021-12-13 13:16:08 +0300

    wsgi fix 4

--- ATE/ATE/wsgi.py
+++ ATE/ATE/wsgi.py
@@ -9,11 +9,10 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 
 
 import os
-import sys
-sys.path.append('/ATE')
-sys.path.append('/ATE/ATE')
-
 from django.core.wsgi import get_wsgi_application
+
+here = os.path.dirname(__file__)
+wsgi = os.path.join(here, 'wsgi.py')
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ATE.settings')
 
