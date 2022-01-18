e7105bd37849376248143d4ad18aa8917ffbdcc6
Barissa-Imran <imranbarissa@gmail.com>
2022-01-19 00:48:04 +0300

    made dashboards mobile responsive
    add bootstrap collapse, fix date calculation issue in Jquery-place order page, modularized writer dashboard, fixed bugs

--- ATE/ace_the_essay/static/js/writer.js
+++ ATE/ace_the_essay/static/js/writer.js
@@ -1,3 +1,29 @@
+// applicant dropdown button
+
+$(document).ready(function () {
+  $(".dropdown-toggle").dropdown();
+});
+
+// sidebar panel show/hide on screen size
+
+$(document).ready(function () {
+  // variable assignment outside event handler
+  var $window = $(window);
+  var $panel = $("#navcol-1");
+
+  function checkWidth() {
+    var windowsize = $window.width();
+    if (windowsize <= 1024) {
+      // if window is equal to tablet ir smaller then hide sidebar panel
+      $panel.toggleClass("collapse");
+    }
+  }
+  //   Execute on load
+  checkWidth();
+  //   Bind event listener
+  $(window).resize(checkWidth);
+});
+
 // $(document).ready(function () {
 //   $(".bid").click(function () {
 //     $.ajax({
--- ATE/users/static/js/client.js
+++ ATE/users/static/js/client.js
@@ -1,9 +1,29 @@
+// sidebar panel show/hide on screen size
+
+$(document).ready(function () {
+  // variable assignment outside event handler
+  var $window = $(window);
+  var $panel = $("#navcol-1");
+
+  function checkWidth() {
+    var windowsize = $window.width();
+    if (windowsize <= 1024) {
+      // if window is equal to tablet ir smaller then hide sidebar panel
+      $panel.toggleClass("collapse");
+    }
+  }
+  //   Execute on load
+  checkWidth();
+  //   Bind event listener
+  $(window).resize(checkWidth);
+});
+
 // calculate approximate number of words according to spacing
 $("#id_spacing").mouseup(function spacing(e) {
   return e.target.value;
 });
 
-// compute the price as the user types from number of pages
+/* compute the price as the user types from number of pages*/
 $("#id_number_of_pages").keyup(function (e) {
   $("#number_of_pages").html(e.target.value + " pages x (price)");
   $("#total").html("$" + e.target.value * 4.0);
@@ -44,72 +64,42 @@ $("#id_type_of_paper").mouseup(function (e) {
 });
 
 // calculate deadline date according to selected date
-// $.each(
-//   $("deadline_time").click(function () {
-//     if ($(this).text() == "36 Hours") {
-//       console.log("yes");
-//     } else {
-//       console.log("no");
-//     }
-//   })
-// );
-
-// calculate deadline date according to selected date
 $(document).ready(function () {
   $("#deadline_count").html(Date);
 });
 
-$("#deadline_6").click(function (e) {
-  $("#deadline_count").html(new Date(Date.now() + 0.25 * 24 * 60 * 60 * 1000));
+$(
+  "#deadline_6, #deadline_12, #deadline_15, #deadline_24, #deadline_36, #deadline_48, #deadline_3, #deadline_5, #deadline_7, #deadline_10, #deadline_14, #deadline_30, #deadline_60"
+).click(function () {
+  if (this.id == "deadline_6") {
+    $("#deadline_count").html(
+      new Date(Date.now() + 0.25 * 24 * 60 * 60 * 1000)
+    );
+  } else if (this.id == "deadline_12") {
+    $("#deadline_count").html(new Date(Date.now() + 0.5 * 24 * 60 * 60 * 1000));
+  } else if (this.id == "deadline_15") {
+    $("#deadline_count").html(
+      new Date(Date.now() + 0.625 * 24 * 60 * 60 * 1000)
+    );
+  } else if (this.id == "deadline_24") {
+    $("#deadline_count").html(new Date(Date.now() + 1 * 24 * 60 * 60 * 1000));
+  } else if (this.id == "deadline_36") {
+    $("#deadline_count").html(new Date(Date.now() + 1.5 * 24 * 60 * 60 * 1000));
+  } else if (this.id == "deadline_48") {
+    $("#deadline_count").html(new Date(Date.now() + 2 * 24 * 60 * 60 * 1000));
+  } else if (this.id == "deadline_3") {
+    $("#deadline_count").html(new Date(Date.now() + 3 * 24 * 60 * 60 * 1000));
+  } else if (this.id == "deadline_5") {
+    $("#deadline_count").html(new Date(Date.now() + 5 * 24 * 60 * 60 * 1000));
+  } else if (this.id == "deadline_7") {
+    $("#deadline_count").html(new Date(Date.now() + 7 * 24 * 60 * 60 * 1000));
+  } else if (this.id == "deadline_10") {
+    $("#deadline_count").html(new Date(Date.now() + 10 * 24 * 60 * 60 * 1000));
+  } else if (this.id == "deadline_14") {
+    $("#deadline_count").html(new Date(Date.now() + 14 * 24 * 60 * 60 * 1000));
+  } else if (this.id == "deadline_30") {
+    $("#deadline_count").html(new Date(Date.now() + 30 * 24 * 60 * 60 * 1000));
+  } else if (this.id == "deadline_60") {
+    $("#deadline_count").html(new Date(Date.now() + 60 * 24 * 60 * 60 * 1000));
+  }
 });
-
-$("#deadline_12").click(function (e) {
-  $("#deadline_count").html(new Date(Date.now() + 0.5 * 24 * 60 * 60 * 1000));
-});
-
-$("#deadline_15").click(function (e) {
-  $("#deadline_count").html(new Date(Date.now() + 0.625 * 24 * 60 * 60 * 1000));
-});
-
-$("#deadline_24").click(function (e) {
-  $("#deadline_count").html(new Date(Date.now() + 1 * 24 * 60 * 60 * 1000));
-});
-
-$("#deadline_36").click(function (e) {
-  $("#deadline_count").html(new Date(Date.now() + 1.5 * 24 * 60 * 60 * 1000));
-});
-
-$("#deadline_48").click(function (e) {
-  $("#deadline_count").html(new Date(Date.now() + 2 * 24 * 60 * 60 * 1000));
-});
-
-$("#deadline_3").click(function (e) {
-  $("#deadline_count").html(new Date(Date.now() + 3 * 24 * 60 * 60 * 1000));
-});
-
-$("#deadline_5").click(function (e) {
-  $("#deadline_count").html(new Date(Date.now() + 5 * 24 * 60 * 60 * 1000));
-});
-
-$("#deadline_7").click(function (e) {
-  $("#deadline_count").html(new Date(Date.now() + 7 * 24 * 60 * 60 * 1000));
-});
-
-$("#deadline_10").click(function (e) {
-  $("#deadline_count").html(new Date(Date.now() + 10 * 24 * 60 * 60 * 1000));
-});
-
-$("#deadline_14").click(function (e) {
-  $("#deadline_count").html(new Date(Date.now() + 14 * 24 * 60 * 60 * 1000));
-});
-
-$("#deadline_40").click(function (e) {
-  $("#deadline_count").html(new Date(Date.now() + 40 * 24 * 60 * 60 * 1000));
-});
-
-$("#deadline_60").click(function (e) {
-  $("#deadline_count").html(new Date(Date.now() + 60 * 24 * 60 * 60 * 1000));
-});
-// id_number_of_pages
-// hint_id_number_of_pages
-// hint_id_deadline
